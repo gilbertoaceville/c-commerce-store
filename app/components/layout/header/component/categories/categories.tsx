0e7dbd8292d210a88c2f9669137b033456ffc334
gilbertoaceville <gilbertoaceville@gmail.com>
2024-02-22 13:19:50 +0100

    feat: complete categories refactoring

--- app/components/layout/header/component/categories/categories.tsx
+++ app/components/layout/header/component/categories/categories.tsx
@@ -1,19 +1,41 @@
 "use client";
 
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from "react-icons/md";
+import clsx from "clsx";
+
+import useToggle from "@/base/hooks/useToggle";
 import { productCategories } from "@/base/utils/constants/const";
 import CategoryItem from "./component/category-item";
 import { usePathname, useSearchParams } from "next/navigation";
 
 export default function HeaderCategories() {
   const params = useSearchParams();
-  const category = params?.get("category");
   const pathname = usePathname();
+  const { isOn, toggle } = useToggle("on");
+
+  const category = params?.get("category");
 
   //Only show categories on home page
-  if(pathname !== "/") return null;
+  if (pathname !== "/") return null;
 
   return (
-    <div className="bg-secondary w-[200px] rounded-md py-4 pl-4 ml-2 fixed top-1/2 -translate-y-1/2 z-10 flex justify-center">
+    <section
+      className={clsx(
+        "fixed w-[200px] top-1/2 -translate-y-1/2 z-10 flex flex-col justify-center"
+      )}
+    >
+      <div
+        className={clsx(
+          "bg-secondary rounded-md py-4 pl-4 transition-all",
+          isOn ? "transform -translate-x-full" : "ml-2"
+        )}
+      >
+        <div className="flex justify-between items-center">
+          <h3 className="text-xl font-bold">Categories</h3>
+        </div>
         <div className="py-4 flex flex-col items-start gap-y-4 justify-between overflow-x-auto">
           {productCategories.map((cat, index) => {
             const isAll = category === null && cat.label === "All";
@@ -27,6 +49,23 @@ export default function HeaderCategories() {
             );
           })}
         </div>
-    </div>
+      </div>
+
+      <button
+        title="Toggle categories"
+        aria-label="Toggle categories"
+        onClick={toggle}
+        className="rounded py-2 w-[50px] xl:w-[80px] flex justify-center mt-4 outline-none border-none transition-all animate-icon-animation shadow-sm"
+      >
+        {isOn ? (
+          <MdKeyboardDoubleArrowRight
+            size={35}
+            className="animate-move-right"
+          />
+        ) : (
+          <MdKeyboardDoubleArrowLeft size={35} className="animate-move-left" />
+        )}
+      </button>
+    </section>
   );
 }
